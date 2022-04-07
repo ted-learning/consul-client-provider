9652dfb9bd1e3bdd4668d3b2ac57895379e4da79
Zhang Gang <gzhang@thoughtworks.com>
2022-04-07 11:18:35 +0800

    update: add sleep time for api

--- src/main/kotlin/demo/consul/client/provider/controller/HelloWorldController.kt
+++ src/main/kotlin/demo/consul/client/provider/controller/HelloWorldController.kt
@@ -10,6 +10,7 @@ class HelloWorldController {
 
     @GetMapping("/hello")
     fun hello(): Map<String,String>{
+//        Thread.sleep(3_000)
         return mapOf("data" to "Hello, I'm consul-client-provider")
     }
 }
